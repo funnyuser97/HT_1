9a32ce01e43697ee031d673fb7b711a639f2a606
funnyuser97 <bogdan.stupka12@gmail.com>
2017-10-19 20:45:20 +0300

    modify task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,14 @@
-print('Result:')
+
+in_data=input()
+print('Input  data: ',in_data)
+mylist=list(in_data.split(', '))
+
+for i in range(len(mylist)):
+	mylist[i]=int(mylist[i])
+
+mytuple=tuple(mylist)
+
+print('Output: ')
+print('List: ',mylist)
+print('Tuple: ',mytuple)
+
